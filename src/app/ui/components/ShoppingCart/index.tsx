c829d95d458573870ba678e2b5f4e2a488c536f0
STIVEN HERNANDEZ <casper.sa690@gmail.com>
2024-04-20 14:35:04 -0500

    add component shoping cart

---
+++ src/app/ui/components/ShoppingCart/index.tsx
@@ -0,0 +1,26 @@
+import  { ReactElement } from 'react';
+import './style.css';
+import { useShoppingCart } from '../../../core/hooks/useShoppingCart';
+
+
+const ShoppingCart = (): ReactElement => {
+  const { shoppingCart, handleQuote } = useShoppingCart();
+    return (
+        <section>
+            <h2>Carrito de compras</h2>
+              {shoppingCart.length > 0 ? (
+                <ul>
+                  {shoppingCart.map((book) => (
+                    <li key={book.id}>
+                      Libro ID: {book.id} - Cantidad: {book.quantity}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No hay libros en el carrito</p>
+              )}
+            <button onClick={handleQuote}>Cotizar</button>
+        </section>
+        );
+  };
+  export default ShoppingCart;
